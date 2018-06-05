570da7308e3949c9a18df48b68047c04198389a3
Alina <tusia95@users.noreply.github.com>
2018-06-05 16:59:17 +0300

    ready homework

--- homework0.php
+++ homework0.php
@@ -1,9 +1,11 @@
-﻿<?php
-/**
- * Created by PhpStorm.
- * User: Роман Бурмистров
- * Date: 05.06.2018
- * Time: 16:21
- */
-
-google bum
+﻿
+<?php
+const TESTCONSTANTA = 'asd';
+$user_name = "Igor";
+function show_something()
+{
+    return 'SOMETHING';
+}
+if (1==1) {
+    echo 'hi';
+}
